bcae7538cfb0eccd97fd4b3b22378ca6c041ef3e
MarkAnthony9014 <markanthony9014@gmail.com>
2022-10-12 16:03:01 -0500

    snapshot updates

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,69 +1,46 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-
-
 router.get('/', (req, res) => {
-  Comment.findAll({
-    attributes: [
-        'id',
-        'comment_text',
-        'user_id',
-        'post_id',
-        [sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id'), 'comment_count']
-    ],
-    order: [['created_at', 'DESC']],
-    include: [
-        {
-            model: Post,
-            attributes: ['title']
-        }
-    ]
-  })
-  .then (dbCommentData => res.json(dbCommentData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-
-
 router.post('/', (req, res) => {
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
   Comment.create({
     comment_text: req.body.comment_text,
     user_id: req.body.user_id,
     post_id: req.body.post_id
   })
-    .then (dbCommentData => res.json(dbCommentData))
+    .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
+      console.log(err);
+      res.status(400).json(err);
     });
 });
-
-
 
 router.delete('/:id', (req, res) => {
   Comment.destroy({
     where: {
-        id: req.params.id
+      id: req.params.id
     }
   })
-  .then(dbCommentData => {
-    if (!dbCommentData) {
-        res.status(404).json({ message: 'No comment found with this id'});
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
         return;
-    }
-    res.json(dbCommentData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+      }
+      res.json(dbCommentData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
-
-
-
 
 module.exports = router;
