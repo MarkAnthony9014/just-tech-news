8b609278d2ca7f23c472ca91f86cf38f30edb2bc
MarkAnthony9014 <markanthony9014@gmail.com>
2022-10-28 15:36:14 -0500

    finished partials

--- server.js
+++ server.js
@@ -19,12 +19,16 @@ const sess = {
   })
 };
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.use(session(sess));
 
-const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
